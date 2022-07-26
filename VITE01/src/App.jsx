60e64eff1fd09bdeb9f03c49297a94be4d258998
Andres17X <andresaragon2001@gmail.com>
2022-07-26 21:13:36 +0200

    v1.0

---
+++ VITE01/src/App.jsx
@@ -0,0 +1,16 @@
+import Bola from "./components/Bola";
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola</h1>
+      <Bola numero="1"/>
+      <Bola numero="2"/>
+      <Bola numero="33git"/>
+    </>
+  )
+}
+
+export default App
