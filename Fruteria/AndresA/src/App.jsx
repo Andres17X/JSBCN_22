74db3238545efda58f4c393df5ade550c60e386a
Andres17X <andresaragon2001@gmail.com>
2022-08-04 13:52:52 +0200

    v2.0

---
+++ Fruteria/AndresA/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Productos from './components/Productos'
+
+function App() {
+
+  return (
+  <>
+      <Productos />
+      
+  </>
+  )
+}
+
+export default App
