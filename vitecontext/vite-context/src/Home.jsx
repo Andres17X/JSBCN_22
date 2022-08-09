d5ee84e4e7524be133af35effd20f858089a4bec
Andres17X <andresaragon2001@gmail.com>
2022-08-09 04:04:23 +0200

    casa

--- vitecontext/vite-context/src/Home.jsx
+++ vitecontext/vite-context/src/Home.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
+import {useContext} from "react";
+import TraductorContext from "./TraductorContext.js";
+import './Home.css';
+import homejpg from './fotos/home.jpg';
 
 function Home(){
+  const Traductor = useContext(TraductorContext);
 
     return (
         <>
+        <h1 className="HomeH1">Parcs de Barcelona</h1>
+        <div className="TextoP">
+        <p>{Traductor.traduce('Home')} {' '} </p>
+        <img src={homejpg}></img>
+        </div>
+
         </>
   );
 }
