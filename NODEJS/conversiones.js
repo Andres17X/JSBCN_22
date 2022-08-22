57b583f2db66ffbc6a4357fab0b047a385bce09b
Andres17X <andresaragon2001@gmail.com>
2022-08-22 13:57:38 +0200

    v2

---
+++ NODEJS/conversiones.js
@@ -0,0 +1,13 @@
+function km_millas(m){
+    return m * 1.609;
+}
+function kilometros_m(km){
+    return km * 1.609;
+}
+
+function celsius_fahrenheit(c){
+    return (c*9/5)+32;
+}
+
+export default {km_millas, kilometros_m, celsius_fahrenheit};
+
---
+++ NODEJS/index.js
@@ -0,0 +1,9 @@
+import conversiones from "./conversiones.js";
+let km = 30;
+let millas = conversiones.km_millas(km);
+
+let celsius = 28;
+let fahrenheit = conversiones.celsius_fahrenheit(celsius);
+console.log(fahrenheit)
+
+console.log(millas)
